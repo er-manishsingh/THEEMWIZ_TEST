f0ecde90ed8a3effc9e8871c3ed33a2367c24184
Manish Singh <ermanishsingh25@gmail.com>
2025-08-17 14:06:48 +0530

    build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import About from "../components/About/About";
 import Banner from "../components/banner/Banner";
 import Portfolio from "../components/portfolio/Portfolio";
 import Pricing from "../components/pricing/Pricing";
 import Faq from "../components/faq/Faq";
 import Contact from "../components/contact/Contact";
+import About from "../components/about/About";
 
 function Home() {
   return (
